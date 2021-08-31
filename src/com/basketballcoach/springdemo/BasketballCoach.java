6eec09e9d84ad85899f0f9e9040846a7dd3135c3
Judy Lian <judylime9@gmail.com>
2021-08-30 23:54:15 -0700

    Add the first version of source code;

---
+++ src/com/basketballcoach/springdemo/BasketballCoach.java
@@ -0,0 +1,16 @@
+package com.basketballcoach.springdemo;
+
+public class BasketballCoach implements Coach{
+	
+	@Override
+	public String getDailyWorkout() {
+		return "Spend 60 minutes on shooting practice";
+	}
+
+	@Override
+	public String getDailyFortune() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
---
+++ src/com/basketballcoach/springdemo/FortuneService.java
@@ -0,0 +1,6 @@
+package com.basketballcoach.springdemo;
+
+public interface FortuneService {
+
+	public String getFortune();
+}
